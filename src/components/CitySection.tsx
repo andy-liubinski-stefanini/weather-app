12ea607ed4a260eedf27dd855f9277758a8753d2
andy <andrei.liubinski@stefanini.com>
2024-07-18 16:21:46 +0300

    feat(UI): static HTML

---
+++ src/components/CitySection.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const CitySection = () => {
+  return <div>CitySection</div>;
+};
+
+export default CitySection;
---
+++ src/components/ForecastBox.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const ForecastBox = () => {
+  return <div>ForecastBox</div>;
+};
+
+export default ForecastBox;
