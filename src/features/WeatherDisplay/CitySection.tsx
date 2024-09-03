bd5f308531fab343ef3c557d0219229c64ac629b
andy <andrei.liubinski@stefanini.com>
2024-09-03 11:38:13 +0300

    WIP

--- src/features/WeatherDisplay/CitySection.tsx
+++ src/features/WeatherDisplay/CitySection.tsx
@@ -1,31 +1,27 @@
 import { Navigation, Search } from '../NavBar';
 import { AppContext } from '../../store';
 import { useContext } from 'react';
-import './styles.scss'; 
+import './styles.scss';
 
 export const CitySection: React.FC = () => {
   const { searchFieldVisible, weatherData, formattedDates } = useContext(AppContext);
 
   const weather = weatherData?.[0] ?? null;
-  const cityCountry = weather
-    ? `${weather.cityName}, ${weather.countryCode}`
-    : 'Loading...';
-  const today =
-    formattedDates.length > 0
-      ? `${formattedDates[0].formattedWeekday}, ${formattedDates[0].formattedDate}`
-      : '';
+  const cityCountry = weather ? `${weather.cityName}, ${weather.countryCode}` : 'Loading...';
+  const today = formattedDates.length > 0 ? `${formattedDates[0].formattedWeekday}, ${formattedDates[0].formattedDate}` : '';
 
   return (
     <section className="main--city">
       <Navigation />
       <div className="city--display_box">
-        <h1 hidden={searchFieldVisible} className='city_box--city_name'>
+        <h1
+          hidden={searchFieldVisible}
+          className="city_box--city_name"
+        >
           {weatherData ? cityCountry : `City, World`}
         </h1>
         <Search />
-        <h2 className="city_box--date">
-          {today}
-        </h2>
+        <h2 className="city_box--date">{today}</h2>
       </div>
     </section>
   );
